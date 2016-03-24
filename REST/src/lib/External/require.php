fa654244ead1200cb1fe61bb835683381c74f8ec
Santeri Hetekivi <santeri@hetekivi.com>
2016-03-24 14:47:57 +0200

    Case sensitivity.

---
+++ REST/src/lib/External/require.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Santeri Hetekivi
+ * Date: 12.3.2016
+ * Time: 12.24
+ */
+
+/**
+ * Require file for external libs.
+ */
+
+require 'kint/Kint.class.php';
+
